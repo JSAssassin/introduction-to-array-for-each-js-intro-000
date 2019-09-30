42af63cfa77ff3c64ef10e4656c5179f34d7e378
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-30 12:35:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your doToElementsInArray() function here:
-function doToElementsInArray(callback){
-  Array.prototype.forEach(callback());
+function doToElementsInArray(array, callback){
+  array.forEach(callback());
 }
 
 // Add your changeCompletely() function here:
